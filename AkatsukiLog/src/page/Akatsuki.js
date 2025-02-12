43d7d256c5cefc3b11225a6defdf2aceb063a328
alessonJuley <alessonabao@gmail.com>
2025-02-12 21:46:27 +1300

    Created content skeleton page

---
+++ AkatsukiLog/src/page/Akatsuki.js
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+
+const Akatsuki = () => {
+
+          return(
+                    <div className="container">
+                              
+                    </div>
+          );
+}
+
+export default Akatsuki;
